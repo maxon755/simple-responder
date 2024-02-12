63b17078bfaa28083aa655b0b8976b321be5a1e2
Maksym Haiduk <maksym.haiduk@gmail.com>
2024-02-12 23:25:06 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := flag.Int("port", 8080, "The port to start an application")
+	responseStatus := flag.Int("status", 200, "The status to respond with")
+	responseBody := flag.String("body", "OK", "The body to resnsond with")
+	contentFilePath := flag.String("body-file", "", "The path to file with response content")
+	responseDelay := flag.Int("delay", 0, "The delay before response. Usefull for timeout emulation")
+
+	flag.Parse()
+
+	if *contentFilePath != "" {
+		responseContentFromFile, err := readFileContent(*contentFilePath)
+		if err != nil {
+			fmt.Println("Could not read file: " + err.Error())
+			return
+		}
+
+		*responseBody = responseContentFromFile
+	}
+
+	r := gin.Default()
+	r.GET("/*proxyPath", func(c *gin.Context) {
+		time.Sleep(time.Duration(*responseDelay) * time.Second)
+
+		c.String(*responseStatus, *responseBody)
+	})
+	r.Run(fmt.Sprintf("0.0.0.0:%d", *port))
+}
+
+func readFileContent(filePath string) (string, error) {
+	content, err := os.ReadFile(filePath)
+	if err != nil {
+		return "", err
+	}
+
+	return string(content), nil
+}
